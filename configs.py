dcf20677abccee9a1cabc43e6400203a6cdae479
winnervetri06 <65523392+tamilvetri@users.noreply.github.com>
2022-04-15 10:24:23 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	API_ID = int(4699897)
+	API_HASH ="0c7a855b705fd743ac3b45ad477b3a21"
+	BOT_TOKEN = "2036089449:AAHvqXGjdMe2zH8dfIFOYXOhvVrvJ89P2kY"
+	BOT_USERNAME ="Gsearch_Vbot"
+	DB_CHANNEL = "-1001499452073"
+	BOT_OWNER = int(1627847685)
+	DATABASE_URL = "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.jqtze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL ="-1001499452073"
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
